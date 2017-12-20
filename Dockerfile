4543d20bb2a8439498cc115c8bf41f5205157351
Arturo Martínez <dunkelheit@gmail.com>
2017-12-20 23:58:39 +0100

    feat: initial project setup

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM mhart/alpine-node:8
+
+ADD package.json /tmp/package.json
+
+RUN cd /tmp && \
+    npm install --production && \
+    rm -f ~/.npmrc
+
+RUN mkdir -p /opt/app && cp -a /tmp/node_modules /opt/app
+
+WORKDIR /opt/app
+ADD . /opt/app
+
+CMD ["node", "."]
---
+++ config/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const convict = require('convict');
+
+const config = convict({
+    log: {
+        name: {
+            format: 'String',
+            default: 'samantha',
+            env: 'LOG_NAME'
+        },
+        level: {
+            format: 'String',
+            default: 'debug',
+            env: 'LOG_LEVEL'
+        }
+    },
+    slack: {
+        token: {
+            format: 'String',
+            default: null,
+            env: 'SLACK_TOKEN'
+        },
+        name: {
+            format: 'String',
+            default: 'Samantha',
+            env: 'SLACK_BOT_NAME'
+        }
+    },
+    amqp: {
+        host: {
+            doc: 'The host of the RabbitMQ service.',
+            format: String,
+            default: null,
+            env: 'AMQP_HOST'
+        },
+        username: {
+            doc: 'A RabbitMQ username.',
+            format: String,
+            default: null,
+            env: 'AMQP_USERNAME'
+        },
+        password: {
+            doc: 'Password for the RabbitMQ user.',
+            format: String,
+            default: null,
+            env: 'AMQP_PASSWORD'
+        },
+        queue: {
+            doc: 'Queue that Samantha will be listening to',
+            format: String,
+            default: null,
+            env: 'AMQP_QUEUE'
+        },
+        retry: {
+            maxTries: {
+                doc: 'Maximum amount of times we will try to (re)connect to AMQP.',
+                format: 'int',
+                default: 10,
+                env: 'AMQP_RETRY_MAX_TRIES'
+            },
+            interval: {
+                doc: 'Initial wait time between attempts in milliseconds.',
+                format: 'int',
+                default: 1000,
+                env: 'AMQP_RETRY_INTERVAL'
+            },
+            backoff: {
+                doc: 'Increase interval by this factor between attempts.',
+                format: 'int',
+                default: 2,
+                env: 'AMQP_RETRY_BACKOFF'
+            }
+        }
+    }
+});
+
+config.validate();
+
+module.exports = config;
